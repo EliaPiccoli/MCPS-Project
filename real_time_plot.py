806c5f6c5095a0919d61ebc54a07872c44d52e6e
Dome00 <tupputi.domenico@gmail.com>
2022-05-07 13:19:53 +0200

    size plot

--- real_time_plot.py
+++ real_time_plot.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 from db import DBPATH
 
 dev2pos = {
-    'kitchen':(1230, 90),
-    'bedroom1':(1235, 985),
-    'bedroom2':(135, 1415),
-    'bedroom3':(135, 720),
-    'bathroom':(135, 565),
+    'kitchen': (1230, 90),
+    'bedroom1': (1235, 985),
+    'bedroom2': (135, 1415),
+    'bedroom3': (135, 720),
+    'bathroom': (135, 565),
 }
 
 dbcon = db.create_connection(DBPATH)
 img = plt.imread("images/casa_2.png")
 plt.figure(figsize=(9, 8), dpi=100)
+plt.subplots_adjust(0.037, 0, 1, 1)
 while True:
     last_temp = db.get_last_temp(dbcon)
     plt.clf()
@@ -20,5 +21,5 @@ while True:
     plt.imshow(img)
     for t in last_temp:
         plt.annotate(f"{t[1]}", xy=dev2pos[t[0]])
-    plt.show(block=True)
+    plt.show(block=False)
     plt.pause(1)
